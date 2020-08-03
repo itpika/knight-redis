af519e62316b13185d60f436bb37a0b6e0036ef9
pk <pk>
2020-08-03 16:16:46 +0800

    加入electron

---
+++ src/startup.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow,ipcMain } = require('electron')
+
+function createWindow () {   
+  // 创建浏览器窗口
+  let win = new BrowserWindow({
+    width: 1400,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // 加载index.html文件
+  win.loadFile('dist/index.html')
+  //当所有窗口都被关闭后退出
+    app.on('window-all-closed', () => {
+    // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
+    // 否则绝大部分应用及其菜单栏会保持激活。
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+  })
+  app.on('activate', () => {
+    // 在macOS上，当单击dock图标并且没有其他窗口打开时，
+    // 通常在应用程序中重新创建一个窗口。
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  });
+  
+}
+
+app.whenReady().then(createWindow)
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 module.exports = {
 	outputDir: 'dist',
-	publicPath: './'
+	publicPath: './',
+	pages: {
+		index: {
+			// page 的入口
+			entry: 'src/main.js',
+			// 在 dist/index.html 的输出
+			filename: 'index.html'
+		}
+	}
 }
