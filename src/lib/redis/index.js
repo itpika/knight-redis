024bf9b7447f0302f03c47618b367eb544381461
pk <pk>
2020-08-07 17:30:10 +0800

    暂存

--- src/lib/redis/index.js
+++ src/lib/redis/index.js
@@ -1,41 +1,26 @@
 const Redis = require('ioredis')
-
-
+const singal = require('./singal.js')
+const { resolve } = require('core-js/fn/promise')
 // ioredis：https://www.npmjs.com/package/ioredis
 
-const redisOptions = {
-  // redis遇到错误尝试重连，默认只在 READONLY 状态进行重连
-  reconnectOnError: function(err) {
-    console.error('err:--', err.toString())
-    if (err.message.includes('NOAUTH Authentication')) {
-      return false
-    }
-    return false
-  },
-  // 尝试重连机制
-  retryStrategy: function (times) {
-    // console.log(times)
-    const delay = Math.min(times * 50, 2000)
-    return delay
-  }
-}
 const pool = {}
-
 module.exports = {
   connection: async function(conf) {
-    if (pool[conf.time]) {
-      return
-    }
-    if (conf.connectTimeout) redisOptions.connectTimeout = conf.connectTimeout
-    // conf.port = 6381
-    const cli = new Redis({ host: conf.address, port: conf.port, password: conf.passwd }, redisOptions)
-    if (await this.Ping(cli) !== 1) {
-      return 0
-    }
-    cli.auth()
-    pool[conf.time] = cli
-    console.info('%s:%s@%s', conf.address, conf.port, conf.passwd)
-    return 1
+    return new Promise(resolve => {
+      if (pool[conf.time]) {
+        resolve()
+        return
+      }
+      // conf.port = 6381
+      
+      if (await this.Ping(cli) !== 1) {
+        return 0
+      }
+      pool[conf.time] = cli
+      console.info('%s:%s@%s', conf.address, conf.port, conf.passwd)
+      return 1
+    })
+
   },
   disconnect: function(time) {
     if (!pool[time]) {
@@ -43,18 +28,47 @@ module.exports = {
     }
     pool[time].disconnect()
     delete pool[time]
-  },
-  Ping: async function(cli) {
-    return new Promise(resolve => {
-      setTimeout(() => {
-        resolve(0)
-      }, 10000);
-
-      (async() => {
-        if (await cli.ping('ok') === 'ok') {
-          resolve(1)
-        }
-      })()
-    })
   }
 }
+
+async function connect (conf) {
+  return new Promise(resolve => {
+    const cli = new Redis({ host: conf.address, port: conf.port, password: conf.passwd }, {
+      reconnectOnError: function(err) {
+        console.error('err:--', err.toString())
+        if (err.message.includes('NOAUTH Authentication')) {
+          // 需要密码
+          resolve(singal.NO_AUTH)
+          return false
+        } else if (err.message.includes('NOAUTH Authentication')) {
+          // 密码错误
+          resolve(singal.NO_AUTH)
+          return false
+        } else if (err.message.includes('READONLY')) {
+          return true
+        }
+        return false
+      },
+      // 尝试重连机制
+      retryStrategy: function (times) {
+        // console.log(times)
+        const delay = Math.min(times * 50, 2000)
+        return delay
+      },
+      connectTimeout: conf.connectTimeout ? conf.connectTimeout : 10000
+    })
+  })
+}
+async function Ping (cli) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(0)
+    }, 10000);
+
+    (async() => {
+      if (await cli.ping('ok') === 'ok') {
+        resolve(1)
+      }
+    })()
+  })
+},
---
+++ src/lib/redis/singal.js
@@ -0,0 +1,3 @@
+exports.NO_AUTH = 1
+exports.PASSWD_ERROR = 2
+exports.CONNECT_TIMEOUT = 3
