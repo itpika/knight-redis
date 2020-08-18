af0c9956b11bbef4750e2045d1f9fc85efa9666c
dalgurak <dalgural.liao@outlook.com>
2020-08-18 22:02:28 +0800

    光标显示完成

--- src/front/components/host/terminal.vue
+++ src/front/components/host/terminal.vue
@@ -24,8 +24,7 @@ export default {
       return this.$store.state.hostView.current
     },
     perfix() {
-      // return `${this.address}[${this.current.selectDB}] > `
-      return `${this.address}[0]> `
+      return `${this.address}[${this.current.selectDB}]> `
     }
   },
   data: function () {
@@ -34,8 +33,8 @@ export default {
       terminalHeight: 0, // 画布高度
       fontSize: 14, // 字体大小
       lineInterval: 4, // 行间隔
-      imgData: '', // 行间隔
-      textLocation: 0
+      currentData: '', // 当前行文本的内容
+      lineNum: 0
     }
   },
   methods: {
@@ -57,23 +56,42 @@ export default {
     // 获得光标
     getFocus() {
     },
-    writePrifix() {
+    // 绘制行头
+    writePrifix(lineNum) {
       const text = document.getElementById('shell-text-layer').getContext('2d')
       text.fillStyle = '#00cc74'
-      text.font = this.fontSize + 'px Microsoft JhengHei'
+      text.font = this.fontSize + 'px Monaco' // Menlo, Monaco
       text.fillText(this.perfix, 0, this.fontSize + this.lineInterval)
+      this.currentData = this.perfix
+      this.lineNum = lineNum
+      this.writeCursor(text)
+    },
+    // 绘出光标
+    writeCursor() {
+      const cursor = document.getElementById('shell-cursor-layer').getContext('2d')
+      cursor.font = this.fontSize + 'px Monaco' // Menlo, Monaco 设置字体样式
+      cursor.fillStyle = '#fff' // 字体颜色
+      // 绘制光标，计算前面文本的宽度后再绘制,宽：8，高：字体宽度再加 5
+      cursor.fillRect(cursor.measureText(this.currentData).width,
+        this.lineInterval + (this.lineNum - 1) * this.fontSize, 8, this.fontSize + 5)
     }
   },
   mounted() {
+    // 监听宽高变化，同步canvas画布
     window.addEventListener('resize', this.getBodyWidth)
     window.addEventListener('resize', this.getBodyHeight)
     this.$nextTick(() => {
-      this.getBodyWidth()
-      this.getBodyHeight()
+      this.getBodyWidth() // 初始化画布宽
+      this.getBodyHeight() // 初始化画布高
       this.$nextTick(() => {
-        this.writePrifix()
+        this.writePrifix(1) // 绘制前缀头
       })
     })
+  },
+  created() {
+    document.onkeydown = (e) => {
+      console.log(e.key, e.keyCode)
+    }
   },
   watch: {
     // 监听父容器的宽高，动态设置canvas画布宽高
@@ -81,28 +99,42 @@ export default {
       const text = document.getElementById('shell-text-layer')
       const textCtx = text.getContext('2d')
       const cursor = document.getElementById('shell-cursor-layer')
-      // 保存图像
-      let imgData
-      if (old !== 0) imgData = textCtx.getImageData(0, 0, this.terminalWidth, old)
+      const cursorCtx = cursor.getContext('2d')
+      // 保存文本和光标
+      let textData, cursorData
+      if (old !== 0) {
+        textData = textCtx.getImageData(0, 0, this.terminalWidth, old)
+        cursorData = cursorCtx.getImageData(0, 0, this.terminalWidth, old)
+      }
       // 设置画布的宽高
       text.height = val
       cursor.height = val
       // 复原图像
-      if (old !== 0) textCtx.putImageData(imgData, 0, 0)
+      if (old !== 0) {
+        textCtx.putImageData(textData, 0, 0)
+        cursorCtx.putImageData(cursorData, 0, 0)
+      }
     },
     // 监听父容器的宽高，动态设置canvas画布宽高
     terminalWidth(val, old) {
       const text = document.getElementById('shell-text-layer')
       const textCtx = text.getContext('2d')
       const cursor = document.getElementById('shell-cursor-layer')
+      const cursorCtx = cursor.getContext('2d')
       // 保存图像
-      let imgData
-      if (old !== 0) imgData = textCtx.getImageData(0, 0, this.terminalWidth, old)
+      let imgData, cursorData
+      if (old !== 0) {
+        imgData = textCtx.getImageData(0, 0, this.terminalWidth, old)
+        cursorData = cursorCtx.getImageData(0, 0, this.terminalWidth, old)
+      }
       // 设置画布的宽高
       text.width = val
       cursor.width = val
       // 复原图像
-      if (old !== 0) textCtx.putImageData(imgData, 0, 0)
+      if (old !== 0) {
+        textCtx.putImageData(imgData, 0, 0)
+        cursorCtx.putImageData(cursorData, 0, 0)
+      }
     }
   }
 }
